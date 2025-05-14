c2563c2490e25182316b4b66cb2ec7eb25f8cbc3
jaywang931117 <145691084+jaywang931117@users.noreply.github.com>
2025-05-14 15:43:10 +0800

    Add files via upload

---
+++ LAB#14/react-game/src/App.js
@@ -0,0 +1,21 @@
+//import logo from './logo.svg';
+import './App.css';
+import MultiButton from './cgu_multiButton'
+import HelloCGU from './HelloCGU';
+import CGU_Login from './cgu_login';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {CGU_Login()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
